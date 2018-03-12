55e03b06d36c1947f7c37ba672f3d12ed9ecad3b
Jung <zhanggen.jung@gmail.com>
2018-03-12 20:55:36 +0800

    add

--- ginMiddlewareTest.go
+++ ginMiddlewareTest.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"time"
 	"log"
+	"net/http"
+	"fmt"
 )
 
 func MiddleWare() gin.HandlerFunc {
@@ -27,12 +29,39 @@ func MiddleWare() gin.HandlerFunc {
 	}
 }
 
+// 鉴权中间件, 鉴定session_id的合法性
+func authMiddleWare() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if cookies, err := c.Request.Cookie("session_id"); err == nil {
+			value := cookies.Value
+			fmt.Println(value)
+			if value == "123" {
+				c.Next()
+				return
+			}
+		}
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		c.Abort()
+		return
+	}
+}
+
 func main() {
 
 	router := gin.Default()
 
+	// 只针对/before这个路径注册, 其他路径不起作用, 所以将下边的Use注释掉则Use后的语句将报错
+	router.GET("/before", MiddleWare(), func(c *gin.Context) {
+		example := c.MustGet("example")
+		log.Println(example)
+		c.JSON(http.StatusOK, gin.H{
+			"example": example,
+		})
+	})
+
+	// 注册全局
 	router.Use(MiddleWare())
-	router.GET("/logger", func(context *gin.Context) {
+	router.GET("/middleware", func(context *gin.Context) {
 		example := context.MustGet("example")
 		log.Println(example)
 		example, err := context.Get("example")
@@ -41,6 +70,28 @@ func main() {
 			return
 		}
 		log.Println(example)
+	})
+
+	// 因为上边注册了全局中间件, 所以这里可以直接使用
+	router.GET("/testMiddleware", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"example": c.MustGet("example")})
+	})
+
+	// 模拟登陆的页面
+	router.GET("/login", func(c *gin.Context) {
+		cookie := &http.Cookie{
+			Name:     "session_id",
+			Value:    "123",
+			Path:     "/",
+			HttpOnly: true,
+		}
+		http.SetCookie(c.Writer, cookie)
+		c.String(http.StatusOK, "Login successful")
+	})
+
+	// 注册鉴权中间件, 模拟登陆后对保护页面的鉴权(session_id)
+	router.GET("/home", authMiddleWare(), func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "Already enter home page"})
 	})
 
 	router.Run(":8080")
